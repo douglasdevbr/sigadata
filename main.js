a2e5bed351f41ffac040b70c2889249d7e9f3fc7
douglas Silva <douglasdev@gmail.com>
2017-03-25 11:46:39 -0300

    8080 to 80

--- main.js
+++ main.js
@@ -6,6 +6,6 @@ app.use('/site', express.static('public'))
 //   res.send('Hello World!')
 // })
 
-app.listen(8080, function () {
+app.listen(80, function () {
   console.log('Example app listening on port 3000!')
 })
