6a94a2e0395f1c065c71570110a0ba5230e6c844
douglas Silva <douglasdev@gmail.com>
2017-03-25 12:04:54 -0300

    haaaa

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ app.use('/site', express.static('public'))
 // app.get('/', function (req, res) {
 //   res.send('Hello World!')
 // })
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 1337;
 app.listen(port, function () {
   console.log('Example app listening on port 3000!')
 })
