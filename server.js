2a7b1b08ea36f6cfb11a80c675a778c5d9dda88e
douglas Silva <douglasdev@gmail.com>
2018-01-18 22:55:02 -0200

    add 8081

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use('/site', express.static('public'))
 // app.get('/', function (req, res) {
 //   res.send('Hello World!')
 // })
-var port =  3000;
+var port =  8081;
 app.listen(port, function () {
   console.log('Example app listening on port 8081!')
 })
